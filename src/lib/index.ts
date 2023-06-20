cd2e1114c1f3bb3d4b6f1d08899220d969b4b97f
Luca Wlcek <luca@it-rahn.de>
2023-06-20 13:45:07 +0200

    fixed export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,6 @@
 // Reexport your entry components here
-import Tailwind from './Tailwind.svelte';
-
-export { Tailwind };
+export { default as ObjectList } from './inputs/ObjectList.svelte';
+export { default as AdvancedSelect } from './inputs/AdvancedSelect.svelte';
+export { default as StringInput } from './inputs/StringInput.svelte';
+export { default as DraggableContainer } from './containers/DraggableContainer.svelte';
+export { default as SideMenuContainer } from './containers/SideMenuContainer.svelte';
