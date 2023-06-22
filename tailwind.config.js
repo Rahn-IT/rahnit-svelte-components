2bb0d9c398d8f6c9a989c74d30612651c33a3dba
Luca Wlcek <luca@it-rahn.de>
2023-06-22 15:07:39 +0200

    changed to light theme for developing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	theme: {
-		extend: {}
+	theme: {},
+	daisyui: {
+		themes: ['light']
 	},
 	plugins: [require('@tailwindcss/typography'), require('daisyui')]
 };
