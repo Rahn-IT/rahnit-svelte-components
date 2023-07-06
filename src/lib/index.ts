de2f06f45d6d10e37916298350acfd65c68f76a0
Luca Wlcek <luca@it-rahn.de>
2023-07-06 12:04:48 +0200

    added form export

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,6 +3,7 @@ export type * from './inputs/types.js';
 export { default as ObjectList } from './inputs/ObjectList.svelte';
 export { default as AdvancedSelect } from './inputs/AdvancedSelect.svelte';
 export { default as StringInput } from './inputs/StringInput.svelte';
+export { default as Form } from './inputs/Form.svelte';
 export { default as MonthInput } from './inputs/date/MonthInput.svelte';
 export { default as YearInput } from './inputs/date/YearInput.svelte';
 export { default as DraggableContainer } from './containers/DraggableContainer.svelte';
