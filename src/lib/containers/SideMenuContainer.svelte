9f331388b3a370652a608d5828c03530eca3c02d
Luca Wlcek <luca@it-rahn.de>
2023-07-28 13:30:59 +0200

    added control to SideMenuContainer

--- src/lib/containers/SideMenuContainer.svelte
+++ src/lib/containers/SideMenuContainer.svelte
@@ -5,19 +5,27 @@
 
 	let leftOpen = false;
 	let rightOpen = false;
+	export let allowLeft = true;
+	export let allowRight = true;
 </script>
 
 <div class="h-full w-full">
 	<div class="flex h-full w-full flex-row overflow-hidden">
 		{#if $$slots.left}
-			<div class="ease relative h-full transition-all duration-500 {leftOpen ? 'w-1/4' : 'w-0'}">
+			<div
+				class="ease relative h-full transition-all duration-500 {leftOpen && allowLeft
+					? 'w-1/4'
+					: 'w-0'}"
+			>
 				<div class="direction-ltr absolute h-full w-full overflow-x-hidden overflow-y-scroll">
 					<slot name="left" />
 				</div>
 
-				<div class="absolute -right-16 top-4 z-10">
-					<ToggleButton bind:value={leftOpen} off_icon={MenuIcon} on_icon={CloseIcon} />
-				</div>
+				{#if allowLeft}
+					<div class="absolute -right-16 top-4 z-10">
+						<ToggleButton bind:value={leftOpen} off_icon={MenuIcon} on_icon={CloseIcon} />
+					</div>
+				{/if}
 			</div>
 		{/if}
 
@@ -26,14 +34,20 @@
 		</div>
 
 		{#if $$slots.right}
-			<div class="ease relative h-full transition-all duration-500 {rightOpen ? 'w-1/4' : 'w-0'}">
+			<div
+				class="ease relative h-full transition-all duration-500 {rightOpen && allowRight
+					? 'w-1/4'
+					: 'w-0'}"
+			>
 				<div class="absolute h-full w-full overflow-x-hidden overflow-y-scroll">
 					<slot name="right" />
 				</div>
 
-				<div class="absolute -left-16 top-4 z-10">
-					<ToggleButton bind:value={rightOpen} off_icon={MenuIcon} on_icon={CloseIcon} />
-				</div>
+				{#if allowRight}
+					<div class="absolute -left-16 top-4 z-10">
+						<ToggleButton bind:value={rightOpen} off_icon={MenuIcon} on_icon={CloseIcon} />
+					</div>
+				{/if}
 			</div>
 		{/if}
 	</div>
