f40336869ae86a7c6b825cd19db75194697173f2
Luca Wlcek <luca@it-rahn.de>
2023-08-03 13:38:09 +0200

    storybook update

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,10 +5,6 @@ const config: StorybookConfig = {
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
 		'@storybook/addon-interactions',
-		{
-			name: '@storybook/addon-styling',
-			options: {}
-		},
 		'@storybook/addon-svelte-csf'
 	],
 	framework: {
