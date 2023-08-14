f0f7fcf366ab887b3a156aab1682b128b04d12db
Luca Wlcek <luca@it-rahn.de>
2023-08-14 17:00:29 +0200

    started work on ContectMenu

---
+++ src/lib/actions/ContextMenu.stories.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { Meta, Story } from '@storybook/addon-svelte-csf';
+	import AddIcon from '@iconify-icons/mdi/plus.js';
+	import RemoveIcon from '@iconify-icons/mdi/remove.js';
+	import YoutubeIcon from '@iconify-icons/mdi/youtube-play.js';
+	import ContextMenu from './ContextMenu.svelte';
+	import type { Action } from '../inputs/types.js';
+
+	const actions: Action<void>[] = [
+		{
+			key: 'add',
+			icon: AddIcon,
+			name: 'Add',
+			run: async () => {
+				console.log('add');
+			}
+		},
+		{
+			key: 'remove',
+			icon: RemoveIcon,
+			name: 'Remove',
+			run: async () => {
+				console.log('remove');
+			}
+		},
+		{
+			key: 'youtube',
+			icon: YoutubeIcon,
+			name: 'Youtube',
+			run: 'https://www.youtube.com/'
+		}
+	];
+</script>
+
+<Meta title="Actions/ContextMenu" component={ContextMenu} />
+
+<Story name="Primary" let:args>
+	<div class="p-2">
+		<ContextMenu {actions} />
+	</div>
+</Story>
---
+++ src/lib/actions/ContextMenu.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { Action } from '../inputs/types.js';
+	import { Icon } from '../index.js';
+
+	export let actions: Action<void>[];
+</script>
+
+<div class="w-96 overflow-hidden rounded-xl border border-secondary bg-base-100 shadow-xl">
+	{#each actions as action}
+		{#if typeof action.run === 'string'}
+			<a
+				class="flex h-12 w-full flex-row items-center justify-start py-1 text-secondary hover:bg-base-200 hover:text-secondary-focus"
+				href={action.run}
+			>
+				<Icon class="h-full w-auto px-2" icon={action.icon} />
+				<div class="text-base-content">
+					{action.name}
+				</div>
+			</a>
+		{:else}
+			<button
+				class="flex h-12 w-full flex-row items-center justify-start py-1 text-secondary hover:bg-base-200 hover:text-secondary-focus"
+				on:click={() => action.run()}
+			>
+				<Icon class="h-full w-auto px-2" icon={action.icon} />
+				<div class="text-base-content">
+					{action.name}
+				</div>
+			</button>
+		{/if}
+	{/each}
+</div>
